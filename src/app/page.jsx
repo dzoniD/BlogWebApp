0ba587c70ab3bebe09b1acfa9732e26834c967ba
dzoniD <edvard_dantes@hotmail.com>
2023-09-24 00:17:10 +0200

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut
+        diam.{" "}
+      </h1>
+      <Link href={"/"}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut
+        diam. Aliquet enim tortor at auctor urna nunc id cursus. Hendrerit
+        gravida rutrum quisque non tellus orci ac auctor. Lacinia at quis risus
+        sed vulputate odio ut enim. A diam maecenas sed enim ut sem viverra
+        aliquet. Rhoncus urna neque viverra justo nec. Ac tincidunt vitae semper
+        quis lectus nulla at. Sed pulvinar proin gravida hendrerit lectus a.
+        Malesuada proin libero nunc consequat interdum varius sit. Venenatis
+        lectus magna fringilla urna. Viverra orci sagittis eu volutpat odio
+        facilisis mauris sit amet. Aliquam ultrices sagittis orci a scelerisque
+        purus semper eget. Tortor consequat id porta nibh venenatis cras sed
+        felis eget. Vulputate dignissim suspendisse in est ante in nibh mauris
+        cursus. Non consectetur a erat nam at lectus. Lacus suspendisse faucibus
+        interdum posuere lorem ipsum dolor sit. Malesuada pellentesque elit eget
+        gravida cum sociis natoque. Pharetra sit amet aliquam id diam maecenas.
+        Risus pretium quam vulputate dignissim suspendisse in est ante. Nec
+        feugiat in fermentum posuere urna. Sagittis orci a scelerisque purus.
+        Egestas integer eget aliquet nibh praesent tristique magna. Nulla
+        facilisi cras fermentum odio. Massa tempor nec feugiat nisl. Turpis
+        egestas sed tempus urna et pharetra. Non pulvinar neque laoreet
+        suspendisse interdum consectetur libero id faucibus. Vulputate sapien
+        nec sagittis aliquam malesuada. Auctor elit sed vulputate mi sit amet.
+        Odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam.
+        Hac habitasse platea dictumst vestibulum rhoncus est. At consectetur
+        lorem donec massa sapien faucibus et molestie ac. Est pellentesque elit
+        ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.
+        Scelerisque eleifend donec pretium vulputate. Rutrum tellus pellentesque
+        eu tincidunt tortor aliquam nulla facilisi cras. Nunc non blandit massa
+        enim nec. Semper quis lectus nulla at volutpat diam ut. Sit amet
+        volutpat consequat mauris nunc. Sagittis eu volutpat odio facilisis
+        mauris. Id nibh tortor id aliquet lectus proin nibh. Odio morbi quis
+        commodo odio aenean sed adipiscing. Cras pulvinar mattis nunc sed
+        blandit libero volutpat sed cras. Nibh sit amet commodo nulla facilisi.
+        Bibendum neque egestas congue quisque. Laoreet sit amet cursus sit amet.
+        Felis donec et odio pellentesque diam volutpat. Sed egestas egestas
+        fringilla phasellus faucibus. Sollicitudin ac orci phasellus egestas
+        tellus rutrum. Adipiscing diam donec adipiscing tristique risus nec
+        feugiat in fermentum. Quis lectus nulla at volutpat. Nulla at volutpat
+        diam ut venenatis tellus in metus. Turpis egestas pretium aenean
+        pharetra magna ac placerat. Quisque non tellus orci ac auctor augue
+        mauris augue neque. Sagittis purus sit amet volutpat consequat. Nunc
+        scelerisque viverra mauris in aliquam sem fringilla ut morbi. Semper
+        viverra nam libero justo laoreet sit amet cursus. Turpis egestas pretium
+        aenean pharetra magna ac placerat vestibulum. Gravida in fermentum et
+        sollicitudin ac orci phasellus egestas tellus. Odio eu feugiat pretium
+        nibh ipsum consequat nisl. Sit amet tellus cras adipiscing enim. Diam
+        sollicitudin tempor id eu nisl nunc.
+      </Link>
+    </div>
+  );
+}
