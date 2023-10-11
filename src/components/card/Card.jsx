ada0b2703b7047d5a352d648053c503f9b9241a6
dzoniD <edvard_dantes@hotmail.com>
2023-10-11 10:39:47 +0200

    blog post card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image}></Image>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href="/">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
+          quo odio, veniam totam mollitia inventore officiis nemo ex libero
+          aliquam corporis, praesentium dolor soluta incidunt, atque voluptas.
+          Saepe, quam vel!
+        </p>
+        <Link href="/" className={styles.link}>
+          Read more
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
