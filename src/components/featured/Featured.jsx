414554d10a8014f496a1c76b24fcb3628c9f1f35
dzoniD <edvard_dantes@hotmail.com>
2023-10-11 08:18:30 +0200

    featured post design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b> Hey Nikola ovde!</b> Discover my stories and creative ideas!
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image}></Image>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse
+            inventore exercitationem deleniti labore. Nulla iure aliquam
+            aspernatur, architecto impedit ratione, fugit similique velit at
+            quae voluptate nostrum corrupti. Tenetur?
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
