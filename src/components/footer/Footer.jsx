c0141ad8c58279185b00837a1cbd0050706db5e0
dzoniD <edvard_dantes@hotmail.com>
2023-10-16 02:07:19 +0200

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>Dzonis blog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+          maiores optio ipsa, omnis inventore expedita neque rerum iure
+          reprehenderit beatae perspiciatis deleniti, nesciunt nisi! Maiores
+          distinctio deserunt sed neque obcaecati.
+        </p>
+        <div className={styles.icons}>
+          <Image src={"/facebook.png"} alt="" width={18} height={18} />
+          <Image src={"/instagram.png"} alt="" width={18} height={18} />
+          <Image src={"/tiktok.png"} alt="" width={18} height={18} />
+          <Image src={"/youtube.png"} alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href={"/"}>Homepage</Link>
+          <Link href={"/"}>Blog</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href={"/"}>Style</Link>
+          <Link href={"/"}>Fashion</Link>
+          <Link href={"/"}>Coding</Link>
+          <Link href={"/"}>Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href={"/"}>Facebook</Link>
+          <Link href={"/"}>Instagram</Link>
+          <Link href={"/"}>Tiktok</Link>
+          <Link href={"/"}>Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
