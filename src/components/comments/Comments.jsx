738343e3bf1711c83575eaa25aa64d699723f1f0
dzoniD <edvard_dantes@hotmail.com>
2023-11-12 01:42:18 +0100

    created add comment api and handle submit function

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./comments.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -22,20 +22,37 @@ const fetcher = async (url) => {
 export const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
-  const { data, isLoading } = useSwr(
+  //mutate = revalidate
+  const { data, mutate, isLoading } = useSwr(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
-  console.log(status);
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    setDesc("");
+    mutate();
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="Write a comment" className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="Write a comment"
+            className={styles.input}
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
