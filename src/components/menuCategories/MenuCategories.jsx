d2ac3fd758f34ab7e39d2592cfdd5c2df078036a
dzoniD <edvard_dantes@hotmail.com>
2023-11-13 19:39:57 +0100

    mapped fetched categories in menu categories

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -2,30 +2,34 @@ import React from "react";
 import styles from "./menuCategories.module.css";
 import Link from "next/link";
 
-export const MenuCategories = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+export const MenuCategories = async () => {
+  const data = await getData();
   return (
     <div className={styles.categoryList}>
-      <Link
-        href="/blog?cat=style"
-        className={`${styles.categoryItem} ${styles.style}`}
-      >
-        Style
-      </Link>
-      <Link href="/blog" className={`${styles.categoryItem} ${styles.fashion}`}>
-        Fashion
-      </Link>
-      <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
-        Food
-      </Link>
-      <Link href="/blog" className={`${styles.categoryItem} ${styles.travel}`}>
-        Travel
-      </Link>
-      <Link href="/blog" className={`${styles.categoryItem} ${styles.culture}`}>
-        Culture
-      </Link>
-      <Link href="/blog" className={`${styles.categoryItem} ${styles.coding}`}>
-        Coding
-      </Link>
+      {data?.categories?.map((item) => {
+        if (item.title) {
+          return (
+            <Link
+              href={`/blog?cat=${item.slug}`}
+              className={`${styles.categoryItem} ${styles[item.slug]}`}
+            >
+              {item.title}
+            </Link>
+          );
+        }
+      })}
     </div>
   );
 };
